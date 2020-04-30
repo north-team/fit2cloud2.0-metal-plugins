56a54c1032a53226790efdaccc4c97a88cb9fb28
ZJH <junhong.zhou@fit2cloud.com>
2020-05-01 01:00:12 +0800

    [fix] Dell 插件爬取基本信息、CPU 信息

--- fit2cloud-dell-metal-plugin/src/main/java/com/fit2cloud/dell/DellMetalProvider.java
+++ fit2cloud-dell-metal-plugin/src/main/java/com/fit2cloud/dell/DellMetalProvider.java
@@ -7,11 +7,13 @@ import com.fit2cloud.metal.sdk.F2CMetalPlugin;
 import com.fit2cloud.metal.sdk.MetalPluginException;
 import com.fit2cloud.metal.sdk.constants.F2CResourceType;
 import com.fit2cloud.metal.sdk.constants.InitMethod;
+import com.fit2cloud.metal.sdk.constants.PluginConstants;
 import com.fit2cloud.metal.sdk.model.*;
 import com.fit2cloud.metal.sdk.util.DataFormatUtil;
 import com.fit2cloud.metal.sdk.util.HttpUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.net.URLEncoder;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -23,6 +25,9 @@ public class DellMetalProvider extends AbstractMetalProvider {
     //web控制台 Http接口
     private static final String loginUrl = "https://%s/data/login";
     private static final String logoutUrl = "https://%s/data/logout";
+    private static final String overViewUrl = "https://%s/data?get="; //请求硬件数据的通用前缀
+    private static final String cpuUrl = "https://%s/sysmgmt/2012/server/processor"; //获取 cpu
+    private static final String cacheUrl = "https://%s/sysmgmt/2012/server/cache?processor="; //获取当前插槽 cpu 各级缓存大小
 
     private static final ConcurrentHashMap<String, Map<String, String>> headersMap = new ConcurrentHashMap();
 
@@ -50,13 +55,80 @@ public class DellMetalProvider extends AbstractMetalProvider {
         if (login(ipmiSnmpRequestStr)) {
             IPMIRequest request = gson.fromJson(ipmiSnmpRequestStr, IPMIRequest.class);
             Map header = headersMap.get(request.getIp());
-            //:todo https://r720.dahaia123.top/ 账号密码: root/calvin 读取硬件信息 参考inspur
-
             try {
+                //:todo https://r720.dahaia123.top/ 账号密码: root/calvin 读取硬件信息 参考inspur
+                MachineEntity entity = new MachineEntity();
+                List<F2CCpu> cpus = new LinkedList<>(); //cpu
+                List<F2CMemory> memories = new LinkedList<>(); //内存
+                List<F2CPhysicalDisk> disks = new LinkedList<>(); //磁盘
+                List<F2CPmNetworkCard> pmNetworkCards = new LinkedList<>(); //网卡
+
+                //爬取基本数据
+                String url = overViewUrl + "sysDesc,nodeId";
+                String response = HttpUtils.get(String.format(url, request.getIp()), header);
+                JSONObject jsonObject = JSONObject.parseObject(DataFormatUtil.XmlToJson(response)).getJSONObject("root");
+                if(jsonObject.getString("status").equals("ok")){
+                    entity.setName(PluginConstants.DELL + " " +jsonObject.getString("sysDesc"));
+                    entity.setBmcIp(request.getIp());
+                    entity.setBrand(PluginConstants.DELL);
+                    entity.setSerialNo(jsonObject.getString("nodeId"));
+                }
+
+                //  获取 cpu 信息 header 需添加 X_SYSMGMT_OPTIMIZE 参数为 true
+                header.put("X_SYSMGMT_OPTIMIZE", "true");
+                String cpuResponse = HttpUtils.get(String.format(cpuUrl, request.getIp()), header);
+                net.sf.json.JSONObject jsonObjectCpu = net.sf.json.JSONObject.fromObject(cpuResponse);
+                net.sf.json.JSONObject Processor = jsonObjectCpu.getJSONObject("Processor");
+                Iterator cpuIt = Processor.keys();
+                while (cpuIt.hasNext()) {
+                    net.sf.json.JSONObject cpu = Processor.getJSONObject((String) cpuIt.next()); //每个 cpu 的信息
+                    F2CCpu cCpu = new F2CCpu();
+                    cCpu.setProcName(cpu.getString("brand"));
+
+                    String procSocket = (cpu.getString("cache")).substring((cpu.getString("cache")).indexOf("processor=") + 10); //插槽名
+
+                    cCpu.setProcSocket(procSocket);
+                    cCpu.setProcNumCores(String.valueOf(cpu.getInt("core_count")));
+                    cCpu.setProcSpeed(String.valueOf(cpu.getInt("current_speed")) + "MHz");
+                    //获取当前插槽 cpu 各级缓存大小
+                    String param = URLEncoder.encode(procSocket, "UTF-8"); //特殊字符转码
+                    String cacheResponse = HttpUtils.get(String.format(cacheUrl, request.getIp()) + param, header);
+                    net.sf.json.JSONObject jsonObjectCache = net.sf.json.JSONObject.fromObject(cacheResponse);
+                    net.sf.json.JSONObject Cache = jsonObjectCache.getJSONObject("Cache");
+                    Iterator cacheIt = Cache.keys();
+                    int i = 0;
+                    while (cacheIt.hasNext() && i<3) {
+                        net.sf.json.JSONObject cache = Cache.getJSONObject((String) cacheIt.next()); //每级缓存的信息
+                        if(null != cache && cache.getInt("level") == 0){
+                            cCpu.setProcNumL1cache(cache.getInt("max_size") == 0 ? null : String.valueOf(cache.getInt("max_size")));
+                        }
+                        if(null != cache && cache.getInt("level") == 1){
+                            cCpu.setProcNumL2cache(cache.getInt("max_size") == 0 ? null : String.valueOf(cache.getInt("max_size")));
+                        }
+                        if(null != cache && cache.getInt("level") == 2){
+                            cCpu.setProcNumL3cache(cache.getInt("max_size") == 0 ? null : String.valueOf(cache.getInt("max_size")));
+                        }
+                        i++;
+                    }
+                    cpus.add(cCpu);
+                }
+                entity.setCpus(cpus);
+                entity.setCpu(cpus.size());
+
+
+
+                //:todo 内存
+
+
+                //:todo 网卡
+
+
+
                 return null;
 
             } catch (Exception e) {
                 logout(ipmiSnmpRequestStr);
+                logger.error("插件获取物理机信息失败");
             }
 
         }
@@ -137,6 +209,10 @@ public class DellMetalProvider extends AbstractMetalProvider {
                     // 取出请求成功后的 cookie
                     Map<String, String> cookieMap = headersMap.get("login");
                     if(null != cookieMap){
+                        // Dell 还需要把登录请求返回的 ST2 拿到，后续请求需要带上这个参数
+                        String forwardUrl = jsonObject.getString("forwardUrl");
+                        String ST2 = forwardUrl.substring(forwardUrl.indexOf("ST2=") + 4);// ST2= 占 4 个长度
+                        cookieMap.put("ST2", ST2);
                         headersMap.put(ip, cookieMap);
                         headersMap.remove("login");
                         return true;
