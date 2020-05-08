1f3b1bdf6ee43929e6a90ae6cee550229de95fc4
zhangdahai112 <haswell112@outlook.com>
2020-05-08 17:34:22 +0800

    重构

--- fit2cloud-metal-plugin-sdk/src/main/java/com/fit2cloud/metal/sdk/constants/F2CResourceType.java
+++
@@ -1,8 +0,0 @@
-package com.fit2cloud.metal.sdk.constants;
-
-public class F2CResourceType {
-    public static final String VM = "vm";
-    public static final String IP = "ip";
-    public static final String RACKHD_RAID_PAYLOAD = "raid_payload.json";
-    public static final String RACKHD_RAID_DEL_PAYLOAD = "delete_raid_payload.json";
-}
